8b44ae800c8b5d801ec02191711cc37ffdc7f68c
Piti <alupit526@ieselcaminas.org>
2022-12-14 20:48:55 +0100

    Tercer commit (mas metodos, clases y los csv)

---
+++ src/LectorCSV.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+
+/**
+ * @author Jorge Pitarch
+ */
+
+public class LectorCSV {
+
+    public void leerCSV(String archivo) {
+
+        BufferedReader lectura =
+    }
+}
